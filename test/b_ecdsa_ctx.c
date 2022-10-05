d0c26e62478e800cbc99c5cdb2728502f1c1fe37
Joerg Schmidbauer <jschmidb@de.ibm.com>
2022-10-05 16:38:04 +0200

    ECDSA test: add tests for ECDSA sign/verify
    Similar to the AES tests, ECDSA NIST test vectors are downloaded, converted
    into json format, and executed. But as CPACF does not allow specifying the
    random k-value for protected key sign ops, we cannot check locally created
    signatures against known signatures. But we can at least use the given key
    material for sign/verify and verify known signatures with given public keys.
    For ED-DSA tests (ed25519 and ed448) we use known answer tests from RFC 8032
    in a static json file.
    
    Signed-off-by: Joerg Schmidbauer <jschmidb@de.ibm.com>

---
+++ test/b_ecdsa_ctx.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright IBM Corp. 2022
+ *
+ * libzpc is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ */
+
+/*
+ * Build test for ecdsa_ctx.h.
+ */
+#include "zpc/ecdsa_ctx.h"
+#include "zpc/ecdsa_ctx.h"
+
+int b_ecdsa_ctx_not_empty;
