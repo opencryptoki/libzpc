89064f77b0c193596a01a4770d285fedcee9daa3
Joerg Schmidbauer <jschmidb@de.ibm.com>
2025-03-03 13:08:18 +0100

    HMAC: add tests for HMAC support
    Tests for protected-key HMAC cover protected keys retrieved from Ultravisor
    retrievable secrets, random HMAC keys, and HMAC keys created from given
    clear key data.
    
    Tests with random HMAC keys and HMAC keys created from clear key data can be
    performed on a z17 or later. Ultravisor retrievable secrets are only supported
    on z17 or later in a secure execution guest under KVM.
    
    Signed-off-by: Joerg Schmidbauer <jschmidb@de.ibm.com>

---
+++ test/b_hmac.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright IBM Corp. 2025
+ *
+ * libzpc is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ */
+
+/*
+ * Build test for hmac.h.
+ */
+#include "zpc/hmac.h"
+#include "zpc/hmac.h"
+
+int b_hmac_not_empty;
---
+++ test/b_hmac_key.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright IBM Corp. 2025
+ *
+ * libzpc is free software; you can redistribute it and/or modify
+ * it under the terms of the MIT license. See LICENSE for details.
+ */
+
+/*
+ * Build test for hmac_key.h.
+ */
+#include "zpc/hmac_key.h"
+#include "zpc/hmac_key.h"
+
+int b_hmac_key_not_empty;
