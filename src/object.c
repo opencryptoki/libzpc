e0aef8576a006956af7bcba35233cd71dabd41f9
Holger Dengler <dengler@linux.ibm.com>
2026-02-25 14:07:00 +0100

    provider: Add provider-specific key object
    The provider-specific key object structure is shared between the
    provider components and references to the internal zpc-key
    structure(s).
    
    Signed-off-by: Holger Dengler <dengler@linux.ibm.com>

---
+++ src/object.c
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the libzpc project
+#include <string.h>
+#include <openssl/crypto.h>
+
+#include "object.h"
+
+static void _obj_free(struct obj *obj)
+{
+	OPENSSL_free(obj->origin_type);
+	OPENSSL_free(obj->origin_alg);
+	OPENSSL_free(obj->origin_blob.p);
+	OPENSSL_free(obj->origin_pubkey.p);
+	OPENSSL_free(obj->apqns);
+	OPENSSL_free(obj->mkvp);
+
+	OPENSSL_free(obj);
+}
+
+void obj_free(struct obj *obj)
+{
+	if (!obj)
+		return;
+
+	if (__atomic_sub_fetch(&obj->refcnt, 1, __ATOMIC_SEQ_CST))
+		return;
+
+	_obj_free(obj);
+}
+
+struct obj *obj_get(struct obj *obj)
+{
+	if (!obj)
+		return NULL;
+
+	__atomic_fetch_add(&obj->refcnt, 1, __ATOMIC_SEQ_CST);
+	return obj;
+}
+
+struct obj *obj_new(struct provider_ctx *pctx)
+{
+	struct obj *obj;
+
+	obj = OPENSSL_zalloc(sizeof(struct obj));
+	if (!obj)
+		return NULL;
+
+	obj->pctx = pctx;
+
+	return obj_get(obj);
+}
+
+struct obj *obj_dup(const struct obj *osrc)
+{
+	struct obj *odst;
+
+	if (!osrc ||
+	    !(odst = obj_new(osrc->pctx)))
+		return NULL;
+
+	if (osrc->origin_type &&
+	    !(odst->origin_type = OPENSSL_strdup(osrc->origin_type)))
+		goto err;
+
+	if (osrc->origin_alg &&
+	    !(odst->origin_alg = OPENSSL_strdup(osrc->origin_alg)))
+		goto err;
+
+	if (osrc->apqns &&
+	    !(odst->apqns = OPENSSL_strdup(osrc->apqns)))
+		goto err;
+
+	if (osrc->mkvp &&
+	    !(odst->mkvp = OPENSSL_strdup(osrc->mkvp)))
+		goto err;
+
+	if (osrc->origin_blob.p &&
+	    !(odst->origin_blob.p = OPENSSL_memdup(osrc->origin_blob.p,
+						   osrc->origin_blob.plen)))
+		goto err;
+	odst->origin_blob.plen = osrc->origin_blob.plen;
+
+	if (osrc->origin_pubkey.p &&
+	    !(odst->origin_pubkey.p = OPENSSL_memdup(osrc->origin_pubkey.p,
+						     osrc->origin_pubkey.plen)))
+		goto err;
+	odst->origin_pubkey.plen = osrc->origin_pubkey.plen;
+
+	return odst;
+err:
+	obj_free(odst);
+	return NULL;
+}
+
+bool obj_cmp(const struct obj *obj1, const struct obj *obj2)
+{
+	if ((!!obj1 != !!obj2) ||
+	    (OPENSSL_strcasecmp(obj1->origin_alg, obj2->origin_alg) != 0))
+		return false;
+
+	if ((!!obj1->origin_pubkey.p == !!obj2->origin_pubkey.p) &&
+	    (obj1->origin_pubkey.plen == obj2->origin_pubkey.plen))
+		return (memcmp(obj1->origin_blob.p,
+			       obj2->origin_blob.p,
+			       obj1->origin_blob.plen) == 0)
+			? true
+			: false;
+
+	return false;
+}
---
+++ src/object.h
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the libzpc project
+#ifndef _OBJECT_H
+#define _OBJECT_H
+
+#include <stdbool.h>
+
+#include "provider.h"
+
+struct data {
+	size_t plen;
+	unsigned char *p;
+};
+
+struct obj {
+	/* common */
+	unsigned int refcnt;
+	struct provider_ctx *pctx;
+
+	/* zpc keys */
+
+	/* origin path attrs */
+	char *origin_type;
+	char *origin_alg;
+	struct data origin_blob;
+	struct data origin_pubkey;
+
+	/* origin qeuery attrs */
+	char *apqns;
+	char *mkvp;
+
+	bool public_only;
+};
+
+struct obj *obj_new(struct provider_ctx *pctx);
+struct obj *obj_get(struct obj *obj);
+struct obj *obj_dup(const struct obj *obj);
+void obj_free(struct obj *obj);
+bool obj_cmp(const struct obj *obj1, const struct obj *obj2);
+
+#endif /* _OBJECT_H */
