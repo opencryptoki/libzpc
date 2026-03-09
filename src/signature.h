01793f30d11ca0b1f88517bcb9cd0deec9ac64fa
Holger Dengler <dengler@linux.ibm.com>
2026-03-09 12:37:49 -0400

    provider: Add signature algorithms
    Add signature algorithms for sign/verify with ECDSA and EDDSA keys.
    
    Signed-off-by: Holger Dengler <dengler@linux.ibm.com>

---
+++ src/signature.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the libzpc project
+#ifndef _SIGNATURE_H
+#define _SIGNATURE_H
+
+#include <openssl/core.h>
+
+extern const OSSL_ALGORITHM signature_ops[];
+
+#endif /* _SIGNATURE_H */
