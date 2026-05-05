75606e7f8f4b87d6ffe4bfa3ffb35600fbf55523
Ingo Franzki <ifranzki@linux.ibm.com>
2026-05-05 10:22:16 +0200

    pkcs11: Add utility helpers
    Add utils.c with a generic dynamic array (dyn_array) implementation
    used throughout the PKCS#11 layer to store lists of objects and
    sessions without a fixed upper bound.
    
    Signed-off-by: Ingo Franzki <ifranzki@linux.ibm.com>

---
+++ src/pkcs11/utils.c
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the libzpc project
+#include <stdint.h>
+#include <stdlib.h>
+#include "utils.h"
+
+#define DYN_ARRAY_CHUNK_SIZE	32
+
+int dyn_array_init(struct dyn_array *da)
+{
+	if (!da)
+		return 0;
+
+	da->elements = NULL;
+	da->size = 0;
+	da->allocated = 0;
+
+	return 1;
+}
+
+int dyn_array_free(struct dyn_array *da)
+{
+	if (!da)
+		return 0;
+
+	if (da->elements)
+		free(da->elements);
+
+	da->elements = NULL;
+	da->size = 0;
+	da->allocated = 0;
+
+	return 1;
+}
+
+size_t dyn_array_size(const struct dyn_array *da)
+{
+	if (!da)
+		return 0;
+
+	return da->size;
+}
+
+int dyn_array_add(struct dyn_array *da, void *element, size_t *index)
+{
+	void **tmp;
+	size_t new_alloc;
+
+	if (!da)
+		return 0;
+
+	if (da->allocated == 0 || da->allocated <= da->size) {
+		new_alloc = da->allocated + DYN_ARRAY_CHUNK_SIZE;
+		if (new_alloc < da->allocated ||
+		    new_alloc > SIZE_MAX / sizeof(void *))
+			return 0;
+
+		tmp = realloc(da->elements, new_alloc * sizeof(void *));
+		if (!tmp)
+			return 0;
+
+		da->elements = tmp;
+		da->allocated = new_alloc;
+	}
+
+	if (index)
+		*index = da->size;
+
+	da->elements[da->size++] = element;
+	return 1;
+}
+
+int dyn_array_get(const struct dyn_array *da, size_t index, void **element)
+{
+	if (!da || !element)
+		return 0;
+
+	if (index >= da->size)
+		return 0;
+
+	*element = da->elements[index];
+	return 1;
+}
+
+int dyn_array_set(struct dyn_array *da, size_t index, void *element)
+{
+	if (!da)
+		return 0;
+
+	if (index >= da->size)
+		return 0;
+
+	da->elements[index] = element;
+	return 1;
+}
---
+++ src/pkcs11/utils.h
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the libzpc project
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <stddef.h>
+
+struct dyn_array {
+	void **elements;
+	size_t size;
+	size_t allocated;
+};
+
+int dyn_array_init(struct dyn_array *da);
+int dyn_array_free(struct dyn_array *da);
+size_t dyn_array_size(const struct dyn_array *da);
+int dyn_array_add(struct dyn_array *da, void *element, size_t *index);
+int dyn_array_get(const struct dyn_array *da, size_t index, void **element);
+int dyn_array_set(struct dyn_array *da, size_t index, void *element);
+
+#endif
