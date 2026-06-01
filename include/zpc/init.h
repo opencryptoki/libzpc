e55cb6b5d97c869288c48d7a3f66dcb8828962d7
Holger Dengler <dengler@linux.ibm.com>
2026-06-01 12:17:10 +0200

    zpc: init: Introduce explicit constructor/destructor
    The libzpc API is no longer exposed as shared object, so make the
    constructor and destructor explicit. It is now up to the consumer to
    call them.
    
    Signed-off-by: Holger Dengler <dengler@linux.ibm.com>

---
+++ include/zpc/init.h
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// Copyright contributors to the libzpc project
+#ifndef _INIT_H
+#define _INIT_H
+
+void zpc_init(void);
+void zpc_fini(void);
+
+#endif /* _INIT_H */
